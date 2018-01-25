da45daf1c2c8b9a46bc746b829888bbb13fb68e0
Joseph Sobrero <joesobrero@gmail.com>
2018-01-25 14:37:30 -0800

    change profile image and add navigation button functions

--- script.js
+++ script.js
@@ -9,32 +9,104 @@ $(document).ready(function() {
     var edu = $('#edu');
     var work = $('#work');
     var contact = $('#contact');
+    var aboutB = $('#about-b');
+    var eduB = $('#edu-b');
+    var workB = $('#work-b');
+    var contactB = $('#contact-b');
 
     // functions
     function showAbout() {
         about.delay(delay).fadeIn(fadeIn);
     }
-    function closeAbout() {
+    function hideAbout() {
         about.fadeOut(fadeOut);
     }
     function showEdu() {
         edu.delay(delay).fadeIn(fadeIn);
     }
-    function closeEdu() {
+    function hideEdu() {
         edu.fadeOut(fadeOut);
     }
     function showWork() {
         work.delay(delay).fadeIn(fadeIn);
     }
-    function closeWork() {
+    function hideWork() {
         work.fadeOut(fadeOut);
     }
     function showContact() {
         contact.delay(delay).fadeIn(fadeIn);
     }
-    function closeContact() {
+    function hideContact() {
         contact.fadeOut(fadeOut);
     }
 
     // events
+    aboutB.click(function() {
+        switch(view) {
+            case 0:
+                return;
+            case 1:
+                hideEdu();
+                break;
+            case 2:
+                hideWork();
+                break;
+            case 3:
+                hideContact();
+                break;
+        }
+        showAbout();
+        view = 0;
+    });
+    eduB.click(function() {
+        switch(view) {
+            case 0:
+                hideAbout();
+                break;
+            case 1:
+                return;
+            case 2:
+                hideWork();
+                break;
+            case 3:
+                hideContact();
+                break;
+        }
+        showEdu();
+        view = 1;
+    });
+    workB.click(function() {
+        switch(view) {
+            case 0:
+                hideAbout()
+                break;
+            case 1:
+                hideEdu();
+                break;
+            case 2:
+                return;
+            case 3:
+                hideContact();
+                break;
+        }
+        showWork();
+        view = 2;
+    });
+    contactB.click(function() {
+        switch(view) {
+            case 0:
+                hideAbout();
+                break;
+            case 1:
+                hideEdu();
+                break;
+            case 2:
+                hideWork();
+                break;
+            case 3:
+                return;
+        }
+        showContact();
+        view = 3;
+    });
 });
