68d514146a1bfc3103b62845a7d424fe5c663e31
Joseph Sobrero <joesobrero@gmail.com>
2018-01-24 18:05:52 -0800

    set up javascript functions and variables

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
     // variables
+    var view = 0;
+    var fadeIn = 1000;
+    var fadeOut = 650;
+    var delay = 800;
+
+    var about = $('#about');
+    var edu = $('#edu');
+    var work = $('#work');
+    var contact = $('#contact');
+
     // functions
+    function showAbout() {
+        about.delay(delay).fadeIn(fadeIn);
+    }
+    function closeAbout() {
+        about.fadeOut(fadeOut);
+    }
+    function showEdu() {
+        edu.delay(delay).fadeIn(fadeIn);
+    }
+    function closeEdu() {
+        edu.fadeOut(fadeOut);
+    }
+    function showWork() {
+        work.delay(delay).fadeIn(fadeIn);
+    }
+    function closeWork() {
+        work.fadeOut(fadeOut);
+    }
+    function showContact() {
+        contact.delay(delay).fadeIn(fadeIn);
+    }
+    function closeContact() {
+        contact.fadeOut(fadeOut);
+    }
+
     // events
 });
